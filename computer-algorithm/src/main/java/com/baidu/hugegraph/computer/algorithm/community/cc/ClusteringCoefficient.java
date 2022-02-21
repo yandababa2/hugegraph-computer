6b71b45122b54d5225a7c1f31aebf2dca683ad52
yanheling <yanheling>
2022-02-21 10:52:08 +0800

    clustering coefficient modified as triangle count

--- computer-algorithm/src/main/java/com/baidu/hugegraph/computer/algorithm/community/cc/ClusteringCoefficient.java
+++ computer-algorithm/src/main/java/com/baidu/hugegraph/computer/algorithm/community/cc/ClusteringCoefficient.java
@@ -25,11 +25,18 @@ import java.util.Iterator;
 import com.baidu.hugegraph.computer.core.config.Config;
 import com.baidu.hugegraph.computer.core.graph.edge.Edge;
 import com.baidu.hugegraph.computer.core.graph.id.Id;
+import com.baidu.hugegraph.computer.core.graph.id.BytesId;
 import com.baidu.hugegraph.computer.core.graph.value.IdList;
 import com.baidu.hugegraph.computer.core.graph.value.IdSet;
+import com.baidu.hugegraph.computer.core.graph.partition.Partitioner;
 import com.baidu.hugegraph.computer.core.graph.vertex.Vertex;
 import com.baidu.hugegraph.computer.core.worker.Computation;
 import com.baidu.hugegraph.computer.core.worker.ComputationContext;
+import com.baidu.hugegraph.computer.core.graph.id.IdType;
+import com.baidu.hugegraph.computer.core.config.ComputerOptions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * ClusteringCoefficient(CC) algorithm could calculate local & the whole graph:
@@ -50,10 +57,13 @@ import com.baidu.hugegraph.computer.core.worker.ComputationContext;
  *  D represents the degree of current vertex
  */
 public class ClusteringCoefficient implements Computation<IdList> {
+    private Partitioner partitioner;
+    private Map<Id, IdList> messageStorage;
+    private int superedgeThreshold;
 
     @Override
     public String name() {
-        return "clustering_coefficient";
+        return "olap_clustering_coefficient";
     }
 
     @Override
@@ -64,6 +74,12 @@ public class ClusteringCoefficient implements Computation<IdList> {
     @Override
     public void init(Config config) {
         // Reuse triangle count later
+        this.partitioner = config.createObject(
+                        ComputerOptions.WORKER_PARTITIONER);
+        this.partitioner.init(config);
+        this.messageStorage = new HashMap();
+        this.superedgeThreshold = 
+            config.get(ComputerOptions.MIN_EDGES_USE_SUPEREDGE_CACHE);
     }
 
     @Override
@@ -85,9 +101,34 @@ public class ClusteringCoefficient implements Computation<IdList> {
             }
         }
 
+        Map<Integer, Id> partitionMinIdMap = this.findMinId(allNeighbors);
+
         // Send all neighbors of id less than self to neighbors
         for (Id targetId : allNeighbors.values()) {
-            context.sendMessage(targetId, neighbors);
+            if (vertex.numEdges() < this.superedgeThreshold) {
+                context.sendMessage(targetId, neighbors);
+            }
+            else {
+                //new branch, try to save some message to send
+                int partitionId = this.partitioner.partitionId(targetId);
+                Id minId = partitionMinIdMap.get(partitionId);
+
+                if (targetId.equals(minId)) {
+                    byte[] flagByte = new byte[1];
+                    flagByte[0] = 1;
+                    Id flagId = new BytesId(IdType.FLAG, flagByte);    
+                    neighbors.add(0, flagId);
+                    context.sendMessage(targetId, neighbors);
+                } 
+                else {
+                    IdList mapper = new IdList();
+                    byte[] flagByte = new byte[1];
+                    Id flagId = new BytesId(IdType.FLAG, flagByte);
+                    mapper.add(flagId);
+                    mapper.add(minId);
+                    context.sendMessage(targetId, mapper);
+                }
+            }
         }
         vertex.value(value);
     }
@@ -102,6 +143,50 @@ public class ClusteringCoefficient implements Computation<IdList> {
         }
     }
 
+    private IdList cacheOrHit(Id vertexId, IdList list) {
+        if (list.size() == 0) {
+            this.messageStorage.put(vertexId, list);
+            return list;
+        }
+
+        Id id0 = list.get(0);
+        IdType type0 = id0.idType();
+        if (type0 != IdType.FLAG) {
+            return list;
+        }
+
+        if (id0.toString().equals("true")) {
+            //save
+            this.messageStorage.put(vertexId, list);
+            return list;
+        }
+        else {
+            //map
+            Id key = list.get(1);
+            IdList listStored = this.messageStorage.get(key);
+            return listStored;
+        }
+    }
+
+    private Map<Integer, Id> findMinId(IdSet allNeighbors) {
+        Map<Integer, Id> partitionMinIdMap = new HashMap();
+
+        for (Id id : allNeighbors.values()) {
+            int partitionId = this.partitioner.partitionId(id);
+            if (!partitionMinIdMap.containsKey(partitionId)) {
+                partitionMinIdMap.put(partitionId, id);
+            }
+            else {
+                Id minId = partitionMinIdMap.get(partitionId);
+                int status = id.compareTo(minId);
+                if (status < 0) {
+                    partitionMinIdMap.put(partitionId, id);
+                }
+            }
+        }
+        return partitionMinIdMap;
+    }
+
     private Integer triangleCount(ComputationContext context, Vertex vertex,
                                   Iterator<IdList> messages) {
         IdSet allNeighbors = ((ClusteringCoefficientValue) vertex.value())
@@ -111,7 +196,9 @@ public class ClusteringCoefficient implements Computation<IdList> {
             int count = 0;
 
             while (messages.hasNext()) {
-                IdList twoDegreeNeighbors = messages.next();
+                IdList message = messages.next();
+                IdList twoDegreeNeighbors = 
+                       this.cacheOrHit(vertex.id(), message);
                 for (Id twoDegreeNeighbor : twoDegreeNeighbors.values()) {
                     if (allNeighbors.contains(twoDegreeNeighbor)) {
                         count++;
